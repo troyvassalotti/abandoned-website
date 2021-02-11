2c9833570ab9a69d70edbe43c196861d25152160
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-02-10 21:26:39 -0500

    good stuff happening

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 const searchbar = document.getElementById('searchbar');
+const removeSearchbar = () => {
+  searchbar.remove();
+}
 searchbar.addEventListener('click', e => {
-  e.preventDefault;
+  e.preventDefault();
   searchbar.setAttribute('disabled', 'true');
   searchbar.setAttribute('class', 'oops');
-});
+  setTimeout(removeSearchbar, 2000);
+  // TODO: make this more stable, maybe use the animation api with javascript instead of css so the thing gets removed upon animation ending instead of me deciding how long it is
+}, {once: true});
